c7c16aa6d27d7de2ba54892f733b1c82b1d6e4bb
sarakara <gulcinsarankara@hotmail.com>
2022-11-13 17:27:26 -0500

    TicketingAppWithSecurity

---
+++ src/test/java/com/cydeo/ticketingprojectwithsecurity/TicketingProjectWithSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketingprojectwithsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectWithSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
